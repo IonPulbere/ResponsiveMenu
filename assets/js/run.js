6f65cd9a30c2be54970ee515dab22092f90b953e
ion <ionPulbere@icloud.com>
2018-06-08 12:33:07 +0100

    I've added te responsive nav to page

---
+++ assets/js/run.js
@@ -0,0 +1,13 @@
+var toggle= document.getElementById('a-toggle');
+function shownav(){
+    var x = document.getElementById('responsive-nav');
+    if(x.className==='responsive-nav'){
+      x.className+=" unfold";
+    }else{
+      x.className='responsive-nav';
+    }
+
+}
+toggle.addEventListener('click',function(){
+  shownav();
+  });
