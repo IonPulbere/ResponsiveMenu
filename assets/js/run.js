5e79a8235524066687a99a4884fa130a6a4c2407
ion <ionPulbere@icloud.com>
2018-06-11 08:49:16 +0100

    asda

--- assets/js/run.js
+++ assets/js/run.js
@@ -1,15 +1,21 @@
 var toggle= document.getElementById('a-toggle');
 function shownav(){
-
     var x = document.getElementById('responsive-nav');
-    if(x.className==='responsive-nav'){
-      x.className+=" unfold";
+    if(x.className==='responsive-nav'){  // check if there is already a class name
+      x.className+=" unfold"; //add a clas unfold
     }else{
-      x.className='responsive-nav';
+      x.className='responsive-nav'; // set the only one class
     }
-
 }
 toggle.addEventListener('click',function(e){
-e.preventDefault();
+    e.preventDefault();
   shownav();
-  });
+},'false');
+// Prevent All ancor to folow the link============================
+var ancors= document.querySelectorAll('a');
+for(var i =0;i<ancors.length;i++){
+    ancors[i].addEventListener('click',function(e){
+    e.preventDefault();},
+    'false');
+}
+// ================================================================
